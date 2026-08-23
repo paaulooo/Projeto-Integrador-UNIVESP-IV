8abb8fb126fad5fd08cf4a0dc3cae06cfaed21c2
amanda1304 <mand.amanda1304@gmail.com>
2026-08-23 20:19:21 -0300

    #2 Criação de elementos

---
+++ front-end/src/menu/MenuPrincipal.jsx
@@ -0,0 +1,21 @@
+
+export function MenuPrincipal (){
+ return(
+    <>
+        <header>
+            <menu>
+                <ul>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                </ul>
+                <ul>
+                    <li></li>
+                </ul>
+                
+            </menu>
+
+        </header>
+    </>
+ )
+}
