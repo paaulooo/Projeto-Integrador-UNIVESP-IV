a1ab3c03e0e0c6d458d47a994e441fd0bfcf0a3b
amanda1304 <mand.amanda1304@gmail.com>
2026-09-02 17:17:05 -0300

    add redapé, estilo

---
+++ front-end/src/elements/Footer.jsx
@@ -0,0 +1,13 @@
+import "./styles/footer.css"
+
+export function Footer (){
+    return(
+        <>
+        <footer>
+            <p>Projeto Integrador UNIVESP</p>
+            <a href="https://iconoir.com/">https://iconoir.com/</a>
+            <a href="https://www.flaticon.com/br/">https://www.flaticon.com/br/</a>
+        </footer>
+        </>
+    )
+}
